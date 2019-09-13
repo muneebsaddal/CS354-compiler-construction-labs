edae24f3b3e48c02e5c042fbdae4a8a008d2a108
Muneeb Saddal <muneebsaddal@outlook.com>
2019-09-13 21:55:40 +0500

    completed task 3

--- lab 2/task_3.c
+++ lab 2/task_3.c
@@ -1,56 +1,100 @@
 #include<stdio.h>
+#include<ctype.h>
 #include<string.h>
+
+void keyw(char *p);
+int i=0,id=0,kw=0,num=0,op=0;
+
+char keys[32][10]={"auto","break","case","char","const","continue","default",
+"do","double","else","enum","extern","float","for","goto",
+"if","int","long","register","return","short","signed",
+"sizeof","static","struct","switch","typedef","union",
+"unsigned","void","volatile","while"};
 
 int main(){
 
-    char str[50];
-    int check_0 = 0;
-    int check_1 = 0;
-    char operators[50];
-    char identifiers[50];
-    int j = 0;
-    int k = 0;
+    char ch,str[25],seps[15]=" \t\n,;(){}[]#\"<>",oper[]="!%^&*-+=~|.<>/?";
+    int j;
+    char fname[50];
+    FILE *f1;
 
-    printf("Enter a string: ");
-    //scanf("%s" ,str);
-    while(scanf("%[^\n]",str)==1){
-        printf("%s",str);
+    printf("enter file path (drive:\\fold\\filename)\n");
+    scanf("%s",fname);
+    f1 = fopen(fname,"r");
+
+    if(f1==NULL){
+        printf("file not found");
+        return 0;
     }
-    for (int i = 0; str[i]!='\0'; i++) {
-        if(str[i] == ' '){
-            continue;
+    while((ch=fgetc(f1))!=EOF){
+        for(j=0;j<=14;j++){
+            if(ch==oper[j]){
+                printf("%c is an operator\n",ch);
+                op++;
+                str[i]='\0';
+                keyw(str);
+            }
         }
-        else if (((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= '0' && str[i] <= '9') || str[i] == '_')){
-            identifiers[j] = str[i];
-            j++;
-        } 
-        else if(str[i] == '+'){
-            operators[k] = str[i];
-            k++;
+        for(j=0;j<=14;j++){
+            if(i==-1)
+            break;
+            if(ch==seps[j]){
+                if(ch=='#'){
+                    while(ch!='>'){
+                        printf("%c",ch);
+                        ch=fgetc(f1);
+                    }
+                    printf("%c is a header file\n",ch);
+                    i=-1;
+                    break;
+                }
+                if(ch=='"'){
+                    do{
+                        ch=fgetc(f1);
+                        printf("%c",ch);
+                    }
+                    while(ch!='"');
+                    printf("\b is an argument\n");
+                    i=-1;
+                    break;
+                }
+                str[i]='\0';
+                keyw(str);
+            }
         }
-        else if(str[i] == '-'){
-            operators[k] = str[i];
-            k++;
+        if(i!=-1){
+            str[i]=ch;
+            i++;
         }
-        else if(str[i] == '*'){
-            operators[k] = str[i];
-            k++;
-        }
-        else if(str[i] == '/'){
-            operators[k] = str[i];
-            k++;
-        }
-        else if(str[i] == '='){
-            operators[k] = str[i];
-            k++;
-        }
+        else
+            i=0;
     }
-    for (int i = 0; i < strlen(identifiers); i++){
-        printf("%c is an identifier.", identifiers[i]);
-    }
-    for (int i = 0; i < strlen(operators); i++){
-        printf("%c is an operator.", operators[i]);
-    }
+    printf("Keywords: %d\nIdentifiers: %d\nOperators: %d\nNumbers: %d\n",kw,id,op,num);
 
     return 0;
+}
+
+void keyw(char *p){
+    int k,flag=0;
+    for(k=0;k<=31;k++){
+        if(strcmp(keys[k],p)==0){
+            printf("%s is a keyword\n",p);
+            kw++;
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0){
+        if(isdigit(p[0])){
+            printf("%s is a number\n",p);
+            num++;
+        }
+        else{
+            if(p[0]!='\0'){
+                printf("%s is an identifier\n",p);
+                id++;
+            }
+        }
+    }
+    i=-1;
 }
